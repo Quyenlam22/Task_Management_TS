a96df82694bc0d0f4b7ffb9662bb62bc3e48d103
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-02-08 15:37:42 +0700

    Find status and sort

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,9 +3,20 @@ import Task from "../models/task.model";
 
 //[GET] api/v1/tasks
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
+    let find = {
         deleted: false
-    });
+    }
+
+    if(req.query.status){
+        find["status"] = req.query.status;
+    }
+
+    let sort = {};
+    if(req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey.toString()] = req.query.sortValue;
+    }
+
+    const tasks = await Task.find(find).sort(sort);
     
     res.json({
         code: "200",
