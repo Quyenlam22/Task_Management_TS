154ea0697abac1ba3403810f4a07c317cd938499
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-02-08 16:43:00 +0700

    Install CORS

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
 import mainV1Routes from "./api/v1/routes/index.route";
+import cors from "cors";
 
 dotenv.config()
 
@@ -12,6 +13,8 @@ database.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors())
 
 mainV1Routes(app);
 
