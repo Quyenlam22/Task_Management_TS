4ca40005bbb5a5525dcfeb720d26c7c9b85e738b
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-02-08 15:55:12 +0700

    Pagination and search tasks

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Task from "../models/task.model";
+import paginationHelper from "../helpers/pagination";
+import searchHelper from "../helpers/search";
 
 //[GET] api/v1/tasks
 export const index = async (req: Request, res: Response) => {
@@ -7,17 +9,33 @@ export const index = async (req: Request, res: Response) => {
         deleted: false
     }
 
-    if(req.query.status){
+    if (req.query.status) {
         find["status"] = req.query.status;
     }
 
     let sort = {};
-    if(req.query.sortKey && req.query.sortValue) {
+    if (req.query.sortKey && req.query.sortValue) {
         sort[req.query.sortKey.toString()] = req.query.sortValue;
     }
 
-    const tasks = await Task.find(find).sort(sort);
-    
+    //Pagination
+    const countRecords = await Task.countDocuments(find)
+    let objectPagination = paginationHelper({
+        currentPage: 1,
+        limitItems: 2
+    },
+        req.query,
+        countRecords
+    )
+
+    // Search
+    const objectSearch = searchHelper(req.query)
+    if (req.query.keyword) {
+        find["title"] = objectSearch.regex
+    }
+
+    const tasks = await Task.find(find).sort(sort).limit(objectPagination.limitItems).skip(objectPagination.skip);
+
     res.json({
         code: "200",
         message: "Success!",
@@ -28,12 +46,12 @@ export const index = async (req: Request, res: Response) => {
 //[GET] api/v1/tasks/detail/:id
 export const detail = async (req: Request, res: Response) => {
     const id: string = req.params.id;
-    
+
     const task = await Task.findOne({
         _id: id,
         deleted: false
     });
-    
+
     res.json({
         code: "200",
         message: "Success!",
